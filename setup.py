d97e39a362688f15eb696473e43eb5a4104198f1
swauthie <samuel.wauthier@ugent.be>
2021-04-15 15:02:37 +0200

    added requirements

--- setup.py
+++ setup.py
@@ -1,3 +1,15 @@
 from setuptools import setup, find_packages
 
-setup(name='e_maze', version='0.1', packages=find_packages())
+with open("requirements.txt", "r") as req:
+    requirements = req.read().splitlines()
+
+setup(
+    name='e_maze',
+    author="DML group",
+    version='0.1',
+    description="Benchmark set of environments",
+    url="https://github.com/thesmartrobot/ambigym",
+    packages=find_packages(),
+    python_requires=">3.6",
+    install_requires=requirements,
+)
